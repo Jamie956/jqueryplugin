18f40f57292ca2e19f7d3e98d25ff4b24d43264a
Unknown <jamie@justek.us>
2018-07-10 14:21:04 +0800

    update

--- diy/01.js
+++ diy/01.js
@@ -1,15 +1,4 @@
 (function ($) {
-    $.fn.say = function () {
-        this.each(function () {
-            $(this).text("Hello, World!");
-        });
-    }
-    $.fn.demo = function (options) {
-        this.each(function () {
-            $(this).text(options.text);
-            $(this).css('color', options.color)
-        });
-    }
     $.fn.demo2 = function (options) {
         var settings = $.extend({
             text: 'Hello, World!',
@@ -22,13 +11,6 @@
         });
     }
 }(jQuery));
-
-$('#title').say();
-
-$('h3').demo({
-    text: 'Salut, le monde!',
-    color: '#005dff'
-});
 
 $('h3').demo2({
     text: 'Salut, le monde!',
